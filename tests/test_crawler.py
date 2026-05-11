dca40541523503255c7c3bbdab688c5673039cab
Morgan-Harvey <fy22mh@leeds.ac.uk>
2026-05-11 14:36:52 +0100

    added test for crawler

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,26 +1,18 @@
-from src.crawler import crawl, extract_links
-
+from src.crawler import extract_links
 
 def test_extract_links_same_domain():
+    """Links to the same domain should be kept and resolved to absolute URLs."""
     html = """
-    <a href="/page1">Page 1</a>
-    <a href="https://quotes.toscrape.com/page/2/">Page 2</a>
-    <a href="https://google.com">External</a>
+        <a href="/page1">Page 1</a>
+        <a href="https://quotes.toscrape.com/page/2/">Page 2</a>
+        <a href="https://google.com">External</a>
     """
-    
     links = extract_links(html, "https://quotes.toscrape.com")
-    
     assert "https://quotes.toscrape.com/page1" in links
     assert "https://quotes.toscrape.com/page/2/" in links
     assert "https://google.com" not in links
-    
-def test_crawl_page_content():
-    pages = crawl("https://quotes.toscrape.com/", delay=0)
-    
-    for url, content in pages.items():
-        assert isinstance(content, str)
-        assert len(content) > 0
-        
+
+
 def test_extract_links_no_duplicates():
     html = """
     <a href="/page1">A</a>
@@ -30,10 +22,26 @@ def test_extract_links_no_duplicates():
     links = extract_links(html, "https://quotes.toscrape.com")
     
     assert len(links) == 1
+
+def test_extract_links_no_anchors():
+    """A page with no <a> tags should return empty without crashing."""
     
-def test_no_duplicate_pages():
-    pages = crawl("https://quotes.toscrape.com/", delay=0)
+    html = "<html><body><p>No links here</p></body></html>"
     
-    urls = list(pages.keys())
+    links = extract_links(html, "https://quotes.toscrape.com")
     
-    assert len(urls) == len(set(urls))
+    assert links == set()
+
+
+def test_extract_links_anchor_without_href():
+    """<a> tags without href should be skipped, not crash."""
+    
+    html = """
+    <a>Missing href</a>
+    <a href="/valid">Valid</a>
+    """
+    
+    links = extract_links(html, "https://quotes.toscrape.com")
+    
+    assert "https://quotes.toscrape.com/valid" in links
+    assert len(links) == 1
